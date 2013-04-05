19d82bb94702194b6446b1a02cc3418658f08293
Ionuț Arțăriși <mapleoin@lavabit.com>
2013-04-05 22:16:28 +0200

    git init - lein template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to thegame
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/thegame/core_test.clj
@@ -0,0 +1,7 @@
+(ns thegame.core-test
+  (:use clojure.test
+        thegame.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
