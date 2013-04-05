3001d2081f116a5bca46604ca2535f3b65222d45
Ionuț Arțăriși <mapleoin@lavabit.com>
2013-04-06 00:56:08 +0200

    working websocket js

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject thegame "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject thegame "0.1.0"
+  :description "generic server for simple mmo games"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.webbitserver/webbit "0.4.14"]]
+  :main thegame.core)
--- src/thegame/core.clj
+++ src/thegame/core.clj
@@ -1,6 +1,16 @@
-(ns thegame.core)
+(ns thegame.core
+  (:import [org.webbitserver WebServer WebServers WebSocketHandler]
+           [org.webbitserver.handler StaticFileHandler]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main []
+  (doto (WebServers/createWebServer 8080)
+    (.add "/websocket"
+          (proxy [WebSocketHandler] []
+            (onOpen [c] (println "opened" c))
+            (onClose [c] (println "closed" c))
+            (onMessage [c j] (do
+                               (println c j)
+                               (.send c "{\"type\": \"upcase\",
+                                          \"message\": \"caca\"}")))))
+    (.add (StaticFileHandler. "."))
+    (.start)))
