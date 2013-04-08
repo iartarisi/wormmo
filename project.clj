551035460d9c36f1f46cd0ee8b20032d445be40c
Ionuț Arțăriși <iartarisi@suse.cz>
2013-04-08 15:46:56 +0200

    draw simple snake with clojurescript

--- project.clj
+++ project.clj
@@ -4,5 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.webbitserver/webbit "0.4.14"]]
+                 [org.clojure/clojurescript "0.0-1586"]
+                 [org.webbitserver/webbit "0.4.14"]
+                 [com.novemberain/langohr "1.0.0-beta12"]]
+  :source-paths ["src/clj"]
+  :plugins [[lein-cljsbuild "0.2.7"]]
+  :cljsbuild {:builds
+              [{:source-path "src/cljs"
+                :compiler {:output-to "snake.js"
+                           :optimizations :whitespace
+                           :pretty-print true}}]}
   :main thegame.core)
---
+++ src/cljs/snake.cljs
@@ -0,0 +1,40 @@
+(ns snake)
+
+(def element-id "canvas")
+(def ^:const cell-width 10) ;; pixels
+(def ^:const snake-length 5) ;; cells
+
+(def ^:const board {:size 400
+                    :canvas (. js/document (getElementById element-id))
+                    :context (. (. js/document (getElementById element-id))
+                                (getContext "2d"))})
+
+(defn create-snake
+  []
+  (let [start (Math/round (/ (* (Math/random) (- (board :size) cell-width))
+                             cell-width))]))
+
+(defn draw-cell
+  [board color x y]
+  (doto (board :context)
+    (#(set! (. % -fillStyle) color))
+    (.fillRect (* x cell-width) (* y cell-width) cell-width cell-width)
+    (#(set! (. % -strokeStyle) "white"))
+    (.strokeRect (* x cell-width) (* y cell-width) cell-width cell-width)))
+
+(defn draw-board
+  [board]
+  (doto (board :context)
+    (#(set! (. % -fillStyle) "white"))
+    (.fillRect 0 0 (board :size) (board :size))
+    (#(set! (. % -strokeStyle) "black"))
+    (.strokeRect 0 0 (board :size) (board :size))))
+
+(draw-board board)
+(draw-cell board "red" 3 4)
+(draw-cell board "red" 3 5)
+(draw-cell board "red" 4 5)
+(draw-cell board "red" 4 6)
+(draw-cell board "red" 4 7)
+(draw-cell board "red" 4 8)
+(draw-cell board "red" 4 9)
